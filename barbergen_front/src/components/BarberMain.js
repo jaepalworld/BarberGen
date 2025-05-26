dd015e15f0c8d4935e68d7aff9790bd2040fbe05
JWORLD\cjw40 <cjw4032@gmail.com>
2025-05-26 17:07:55 +0900

    main 정리

--- barbergen_front/src/components/BarberMain.js
+++ barbergen_front/src/components/BarberMain.js
@@ -4,9 +4,6 @@ import OneSection from './home/mainsection/OneSection';
 import TwoSection from './home/mainsection/TwoSection';
 import ThreeSection from './home/mainsection/ThreeSection';
 import FourSection from './home/mainsection/FourSection';
-import FiveSection from './home/mainsection/FiveSection';
-import SixSection from './home/mainsection/SixSection';
-import SevenSection from './home/mainsection/SevenSection';
 import TickerSlider from './TickerSlider';
 import Header from './common/Header';
 import Footer from './common/Footer';
@@ -29,17 +26,12 @@ const BarberMain = () => {
             {/* 섹션 3: 예약 섹션 */}
             <ThreeSection />
 
-            {/* 섹션 4: 서비스 소개 */}
+            {/* 섹션 4: 자기 소개 */}
             <FourSection />
 
-            {/* 섹션 5: 고객 후기 */}
-            <FiveSection />
 
-            {/* 섹션 6: 팀 소개 */}
-            <SixSection />
 
-            {/* 섹션 7: 갤러리 */}
-            <SevenSection />
+
 
             {/* 푸터 */}
             <Footer />
